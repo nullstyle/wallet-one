c64586620207affb635f030ff586a7e24950833b
Scott Fleckenstein <nullstyle@gmail.com>
2015-11-05 14:38:56 -0800

    Add bundling and build environments

--- build.js
+++ build.js
@@ -1,23 +1,56 @@
 var Metalsmith = require("metalsmith");
 var $ = require("load-metalsmith-plugins")();
+var Builder = require('systemjs-builder');
+var _ = require("lodash");
 
-Metalsmith(__dirname)
-	.use($.watch({
+const env = process.env.NODE_ENV;
+console.log("NODE_ENV: ", env);
+
+const defaultConfig = {
+	bundle: false,
+	watch: true,
+}
+
+const environments = {
+	production: {
+		bundle: true,
+		watch: false,
+	}
+}
+
+var config = _.extend(defaultConfig, environments[env]);
+console.log("config: ", config);
+
+var builder = Metalsmith(__dirname)
+
+// setup builder middleware
+builder.use($.define({ config }));
+builder.use($.ignore([ "**/.*" ]));
+builder.use($.sass());
+builder.use($.inPlace({ pattern: "*.html", engine: 'ejs' }));
+  
+if (config.watch) {
+	builder.use($.watch({
 		livereload: true,
 		paths: {
 			"${source}/scripts/**/*": true,
 			"${source}/styles/**/*": true,
 			"${source}/*": true,
 		}
-	}))
-	.use($.ignore([
-				"**/.*"
-	]))
-	.use($.sass())
-  .build(function(err) {
-    if (err) throw err;
+	}));
+}
 
-		if (true) {
-			// TODO: build the system.js bundle
-		}
-  });
+// do the actual build
+builder.build(function(err) {
+	if (err) throw err;
+
+	if (config.bundle) {
+		var bundler = new Builder('build', 'build/config.js');
+		bundler.bundle('scripts/main.js', 'build/bundle.js')
+			.catch(function(err) {
+				console.log('bundle error');
+				console.log(err);
+			});
+	}
+});
+
